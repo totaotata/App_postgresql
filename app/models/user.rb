46220308e7960d5a01866101d7c92345687c95af
totaotata <totaotata@protonmail.com>
2019-02-01 16:34:13 +0100

    test OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :username, presence: true
+  validates :username, length: { minimum: 3 }
+  has_many :books
+  def full_name
+      return self.first_name + " " +  self.last_name
+  end
 end
